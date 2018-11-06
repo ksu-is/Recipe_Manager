38ce0173ecbae2da46c26d09a208f0312924c733
Jdraps <42561974+Jdraps@users.noreply.github.com>
2018-11-06 16:31:49 -0500

    Update Planner/models.py
    Sorted importants, added more comments to enhance readability

---
+++ Planner/models.py
@@ -0,0 +1,54 @@
+#Sorted Imports
+from datetime import date, timedelta
+from test.datetimetester import DAY
+
+from _datetime import datetime
+from django.db import models
+from recipes.models import Recipe
+
+
+#Defined Calender Class
+class Calendar(models.Model):
+    name = models.CharField(max_length=250)
+    num_entries_per_day = models.PositiveSmallIntegerField()
+    
+    def entries_num_range(self):
+        return range(0, self.num_entries_per_day)
+    
+    def get_entries(self, start, days):
+        """Returns a full set of entries to display."""
+        result = []
+        
+        for d in (start + timedelta(n) for n in range(0, days)):
+            e = {}
+            e['date'] = d
+            
+            for i in range(0, self.num_entries_per_day+1):
+                e[i] = CalendarEntry.objects.filter(calendar=self).filter(date=d).filter(num_entry=i).first()
+            
+            result.append(e)
+        
+        return result
+    
+    
+    def __str__(self):
+        return self.name    
+    
+    class Meta:
+        ordering = ['name']
+
+
+
+class CalendarEntry(models.Model):
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+    date = models.DateField()
+    num_entry = models.PositiveSmallIntegerField()
+    
+    recipe = models.ForeignKey(Recipe, blank=True, on_delete=models.CASCADE)
+    text = models.CharField(max_length=250, blank=True) 
+    
+    def __str__(self):
+        return '[' + self.date.strftime('%d.%m.%Y') + '] ' + str(self.recipe)
+    
+    class Meta:
+        ordering = ['date', 'num_entry'] 
